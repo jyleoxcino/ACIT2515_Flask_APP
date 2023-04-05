34d794d779b8ffa3a639e660b2e576b641ca401b
Your Name <you@example.com>
2023-04-04 18:22:55 -0700

    updated create_order.py

--- create_order.py
+++ create_order.py
@@ -10,11 +10,11 @@ with app.app_context():
 
     print("New order, with ID", o.id)
 
-    # Let's add five random products with random quantities to the order
-    for x in range(5):
-        products = random.sample(Product.query.all(), k=5)
-        for p in products:
-            quantity = random.randint(1, 10)
-            association = ProductsOrder(product=p, order=o, quantity=quantity)
-            db.session.add(association)
-        db.session.commit()
+    # Let's add five random products with random quantities to the order:
+    products = random.sample(Product.query.all(), k=5)
+    for p in products:
+        quantity = random.randint(1, 10)
+        association = ProductsOrder(product=p, order=o, quantity=quantity)
+        db.session.add(association)
+    
+    db.session.commit()
